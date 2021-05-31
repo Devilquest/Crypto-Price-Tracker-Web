da4d3a8f74409e362d77a2a650d25c63fd23ec94
Marti <martiselldie@gmail.com>
2021-05-31 19:00:31 +0200

    Add project files.

--- README.md
+++ README.md
@@ -1,1 +1,65 @@
-Crypto Price Tracker Web - Git
+# Crypto Price Tracker Web
+**Web to track the profit/loss of a crypto asset.**
+(Only works with [Binance](https://bit.ly/MartiBinance10) trading pairs)
+
+## Configuration
+There are five variables in the `Main.js` file:
+- **tradingPair:** Trading Pair to check the price. (e.g., `BTCUSDT`, `ETHEUR`, `ADABTC`, etc. _Only works with [Binance](https://bit.ly/MartiBinance10) trading pairs_).^1^
+- **buyPrice:** Price at the asset was purchased.
+- **buyQuantity:** Quantity purchased.
+- **currencySymbol:** Currency symbol to display.  (e.g., `$`, `€`, `£`, `BTC`, etc.).^1^ 
+- **updateInterval:** Interval, **in seconds**, of how often the price is updated. (Minimum 5).
+- **decimalPlaces:** Decimal places to display. Usually `2` for traditional currencies and `8` for cryptocurrencies.
+
+1. **String variable:** Place it between quotes or double quotes.
+
+## Examples
+### BTCUSDT
+0.125 Bitcoin purchased at a Bitcoin price of 35245.24$. With an update interval of 10 seconds, showing 2 decimal places.
+```javascript
+var tradingPair = "BTCUSDT";
+var buyPrice = 35245.24;
+var buyQuantity = 0.125;
+var currencySymbol = "$"; 
+var updateInterval = 10;
+var decimalPlaces = 2; 
+```
+
+### ETHEUR
+3.5 Ethereum purchased at a price of 325€ each. With an update interval of 25 seconds, showing 2 decimal places.
+```javascript
+var tradingPair = "ETHEUR";
+var buyPrice = 325;
+var buyQuantity = 3.5;
+var currencySymbol = "€"; 
+var updateInterval = 25;
+var decimalPlaces = 2;
+```
+### ADABTC
+1500 Cardano purchased at a price of 0.00004215 Bitcoin each. With an update interval of 5 seconds, showing 8 decimal places.
+```javascript
+var tradingPair = "ADABTC";
+var buyPrice = 0.00004215;
+var buyQuantity = 1500;
+var currencySymbol = "BTC";
+var updateInterval = 5;
+var decimalPlaces = 8;
+```
+<br>
+
+-------------
+## :heart:Donations
+**Donations are always greatly appreciated. Thank you for your support!**
+
+[![Bitcoin Badge](https://img.shields.io/badge/Bitcoin-89520e?style=flat&logo=bitcoin&logoColor=white)](#heartdonations)
+[![Ethereum Badge](https://img.shields.io/badge/Ethereum-3C3C3D?style=flat&logo=ethereum&logoColor=white)](#heartdonations)
+[![Dogecoin Badge](https://img.shields.io/badge/Dogecoin-C2A633?style=flat&logo=dogecoin&logoColor=white)](#heartdonations)
+[![Litecoin Badge](https://img.shields.io/badge/Litecoin-A6A9AA?style=flat&logo=litecoin&logoColor=white)](#heartdonations)
+
+**BTC:** 1M5yLyLWmELQo2e6chBZ35RkhcTUon66u5
+
+**ETH:** 0xa6d337a733cbce6aa2f0aa72f83beced3464ee86
+
+**DOGE:** DNm19soXsPHHSpqsMUiy75kciw9sBrqeZ8
+
+**LTC:** MKiegbfCssBEJdtNm3tGcnAhBF2xvcQ6d1
---
+++ Scripts/Main.js
@@ -0,0 +1,79 @@
+/*-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-CONFIGURATION_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-*/
+var tradingPair = "BTCUSDT"; //Trading Pair to check the price. (Only Binance trading pairs)
+var buyPrice = 35245.24; //Price the asset was purchased.
+var buyQuantity = 0.125; //Quantity purchased.
+var currencySymbol = "$"; //Currency Symbol.
+var updateInterval = 10; //Price update interval. In seconds. (Minimum 5)
+var decimalPlaces = 2; //Decimal places to display. Usually 2 for traditional currencies and 8 for cryptocurrencies.
+/*-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-*/
+
+$(document).ready(function(){
+	$("#initial-value").text(round(buyPrice * buyQuantity, decimalPlaces).toFixed(decimalPlaces) + currencySymbol);
+	
+	GetBitcoinPrice();
+    
+    if(updateInterval < 5) updateInterval = 5;
+
+	setInterval(function(){ 
+		GetBitcoinPrice();
+	}, updateInterval * 1000);
+});
+
+function GetBitcoinPrice(){
+	$.getJSON( 'https://api.binance.com/api/v3/ticker/price?symbol=' + tradingPair.toUpperCase(), function(data) {
+		var actualValue = buyQuantity * data.price;
+		var difference = actualValue - buyPrice * buyQuantity;
+		var precent = Math.abs((data.price / buyPrice * 100) - 100);
+		
+		$("#actual-value").text(round(actualValue, decimalPlaces).toFixed(decimalPlaces) + currencySymbol);
+		
+		if(difference > 0)
+		{
+			$("#profit-loss-label").text( "Profit:" );
+			
+			$("#profit-loss").removeClass("text-danger");
+			$("#profit-loss").addClass("text-success");
+            
+			$("#percentage").removeClass("text-danger");
+			$("#percentage").addClass("text-success");
+		}
+		else
+		{
+			$("#profit-loss-label").text( "Loss:" );
+			
+			$("#profit-loss").removeClass("text-success");
+			$("#profit-loss").addClass("text-danger");
+            
+			$("#percentage").removeClass("text-success");
+			$("#percentage").addClass("text-danger");
+		}
+		
+		$("#profit-loss").text(round(difference, decimalPlaces).toFixed(decimalPlaces) + currencySymbol);
+		$("#percentage").text(round(precent, 2).toFixed(2) + "%");
+	})
+	.fail(function() {
+		$("#actual-value").text("Error!");
+		$("#profit-loss-label").text("Wrong Trading Pair?");
+		$("#profit-loss").text(tradingPair);
+	});
+}
+
+function round(value, exp) {
+	if (typeof exp === 'undefined' || +exp === 0)
+		return Math.round(value);
+
+	value = +value;
+	exp = +exp;
+
+	if (isNaN(value) || !(typeof exp === 'number' && exp % 1 === 0))
+		return NaN;
+
+	// Shift
+	value = value.toString().split('e');
+	value = Math.round(+(value[0] + 'e' + (value[1] ? (+value[1] + exp) : exp)));
+
+	// Shift back
+	value = value.toString().split('e');
+	return +(value[0] + 'e' + (value[1] ? (+value[1] - exp) : -exp));
+}
+
